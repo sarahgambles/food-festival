174ce45ab430d00c26a3c34610ba1962ab7ca72b
Sarah Gambles <sarahgambles@Sarahs-MacBook-Pro.local>
2020-12-06 15:42:16 -0700

    added webpack

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
+require("bootstrap");
+
 $(document).ready(function() {
+
   function createEl(htmlString, attrs, ...children) {
     if (typeof htmlString !== "string") {
       throw Error("Argument 'htmlString' is required and must be a string");
